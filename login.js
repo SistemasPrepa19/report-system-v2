ca775712dd0a92ab4ed43637f041fc9068674a88
SistemasPrepa19 <sistemasprepa19@gmail.com>
2025-05-27 11:44:10 -0600

    Add login functionality with user validation and redirection

---
+++ login.js
@@ -0,0 +1,14 @@
+function logIn() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+
+    if (name === "") {
+        alert("Por favor, ingresa tu nombre.");
+        return;
+    }  else if (name === "Soporte" && password === "Etropos") {
+        alert("¡Bienvenido, administrador!");
+        window.location.href = "formularioAdmin.html";
+    }  else {
+        window.location.href = "formularioUsuario.html";
+    }
+}
--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const form = document.getElementById("formulario");
-    const estado = document.getElementById("estado");
+const estado = document.getElementById("estado");
 
-    form.addEventListener("submit", e => {
-      e.preventDefault();
-      fetch("https://script.google.com/macros/s/AKfycbwphYi6FTmMHTN4dDcru0R1YeI8L40EKRfqzul0asX37XOtzjLag5CgX0GfRe0M7VU2xQ/exec", {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: new URLSearchParams(new FormData(form))
-      }).then(() => {
-        estado.textContent = "Formulario enviado correctamente.";
-        alert("Formulario enviado correctamente!");
-        form.reset();
-      }).catch(err => {
-        estado.textContent = "Hubo un error al enviar.";
-        alert("Hubo un error al enviar!");
-        console.error(err);
-      });
-    });
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  fetch("https://script.google.com/macros/s/AKfycbwShO6XelDw_pFqqeVEPHZFO_V9J0-U3jARwQO_aPNFsI-eDGyZfWzDsNfc58CO_6l7sw/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams(new FormData(form))
+  })
+  .then(() => {
+    estado.textContent = "Formulario enviado correctamente.";
+    alert("Formulario enviado correctamente!");
+
+    // Espera un momento antes de recargar (opcional)
+    setTimeout(() => {
+      location.reload(); // Recargar la página
+    }, 500); // 0.5 segundos, puedes ajustar el tiempo
+  })
+  .catch(err => {
+    estado.textContent = "Hubo un error al enviar.";
+    alert("Hubo un error al enviar!");
+    console.error(err);
+  });
+});
