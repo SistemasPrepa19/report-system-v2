88bb92a9e2345b506857fdf04dc2e92e79aba713
SistemasPrepa19 <sistemasprepa19@gmail.com>
2025-05-27 12:38:22 -0600

    Enhance form functionality and UI: add success modals, improve styles, and implement user name handling

--- login.js
+++ login.js
@@ -2,11 +2,14 @@ function logIn() {
     var name = document.getElementById("name").value;
     var password = document.getElementById("password").value;
 
+    // Guardar el nombre en localStorage
+    localStorage.setItem("username", name);
+
     if (name === "") {
         alert("Por favor, ingresa tu nombre.");
         return;
     }  else if (name === "Soporte" && password === "Etropos") {
-        alert("¡Bienvenido, administrador!");
+        //alert("¡Bienvenido, administrador!");
         window.location.href = "formularioAdmin.html";
     }  else {
         window.location.href = "formularioUsuario.html";
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const form = document.getElementById("formulario");
-const estado = document.getElementById("estado");
+const modal = document.getElementById("successModal");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
 
-  fetch("https://script.google.com/macros/s/AKfycbwShO6XelDw_pFqqeVEPHZFO_V9J0-U3jARwQO_aPNFsI-eDGyZfWzDsNfc58CO_6l7sw/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbzNmjAiZWYZjaITJzuuAwWAjypkSJX8fsYeoEX08EVy2i4g6lQhON-T18HHX8f_76rTZA/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
@@ -12,17 +12,40 @@ form.addEventListener("submit", e => {
     body: new URLSearchParams(new FormData(form))
   })
   .then(() => {
-    estado.textContent = "Formulario enviado correctamente.";
-    alert("Formulario enviado correctamente!");
+    // Mostrar el modal
+    modal.style.display = "flex";
 
-    // Espera un momento antes de recargar (opcional)
+    // Cerrar automáticamente después de 2 segundos (2000 ms)
     setTimeout(() => {
-      location.reload(); // Recargar la página
-    }, 500); // 0.5 segundos, puedes ajustar el tiempo
+      modal.style.display = "none";
+      window.location.href = "index.html";
+    }, 3000);
   })
   .catch(err => {
-    estado.textContent = "Hubo un error al enviar.";
-    alert("Hubo un error al enviar!");
     console.error(err);
+    alert("Hubo un error al enviar!");
   });
 });
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const storedName = localStorage.getItem("username");
+  if (storedName) {
+    document.getElementById("nombre-usuario").textContent = storedName;
+
+    // Crear también un input hidden para enviar el nombre en el formulario
+    const hiddenInput = document.createElement("input");
+    hiddenInput.type = "hidden";
+    hiddenInput.name = "name";
+    hiddenInput.value = storedName;
+    document.getElementById("formulario").appendChild(hiddenInput);
+  }
+});
+
